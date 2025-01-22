7bdd8dcf3637ca68eb1ac97935293d67a775f4b1
Элиза Галеева <113139305+elissse@users.noreply.github.com>
2025-01-22 11:23:10 +0300

    so the squares are moving now so that's good i guess

--- src/main/java/com/pyatnashki/gui/game/Board.java
+++ src/main/java/com/pyatnashki/gui/game/Board.java
@@ -2,12 +2,17 @@ package com.pyatnashki.gui.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class Board extends JFrame {
-    private JLayeredPane lpane1 = new PlayerBoard(new GameBoard(),0,0);
-    private JLayeredPane lpane2 = new PlayerBoard(new GameBoard(),330,0);
+    private JLayeredPane lpane1 = new PlayerBoard(new GameBoard(new ArrayList<>(asList("0","1","2","3","4","5","6","7","8"))),0,0);
+    private JLayeredPane lpane2 = new PlayerBoard(new GameBoard(new ArrayList<>(asList("0","1","2","3","4","5","6","7","8"))),330,0);
 
-
+    /* key codes:
+    37, 38, 39, 40
+     */
     Board() {
         super();
         add(lpane1, BorderLayout.CENTER);
--- src/main/java/com/pyatnashki/gui/game/MoveListener.java
+++ src/main/java/com/pyatnashki/gui/game/MoveListener.java
@@ -13,6 +13,9 @@ class MoveListener implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         System.out.println("keyPressed " + e);
+        if (e.getKeyCode() == 37 ||e.getKeyCode() == 38 ||e.getKeyCode() == 39 ||e.getKeyCode() == 40) {
+            System.out.println("hey\\");
+        }
     }
 
     @Override
