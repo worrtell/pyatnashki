65fb7f47fff573ba5c515a6771b21d9cb11e1407
Диана Ахмадуллина <diaakhmadullina@gmail.com>
2025-01-23 16:35:43 +0300

    kill me pls

---
+++ src/main/java/com/pyatnashki/controller/listener/ContextListener.java
@@ -0,0 +1,33 @@
+package com.pyatnashki.controller.listener;
+
+import com.pyatnashki.data.User;
+import com.pyatnashki.service.UserService;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+import java.io.IOException;
+import java.util.*;
+
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+        Map<String, User> users = new HashMap<>();
+        ArrayList<User> pairs = new ArrayList<>();
+
+        UserService userService = new UserService();
+
+        servletContext.setAttribute("users", users);
+        servletContext.setAttribute("pairs", pairs);
+        servletContext.setAttribute("userService", userService);
+    }
+    public void contextDestroyed(ServletContextEvent event) {
+        Map userSessions = (Map) event.getServletContext().getAttribute("USER_SESSIONS");
+        userSessions.clear();
+    }
+}
